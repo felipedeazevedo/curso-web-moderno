e2d3952d6bcc65a979ab8795db9098429b371dec
Felipe de Azevedo <felipe.deazevedo@outlook.com>
2022-12-18 02:21:25 -0300

    fundamentos de javascript

---
+++ fundamentos/fundamentos-js/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log (7 / 0.0001);
+console.log("10.2" / 2); // divide pois vai analisar o conteúdo interno da String e verá que é divisível
+console.log("10,2" / 2); // NaN
+console.log("Show!" * 2);
+console.log(0.1 + 0.7); // a especificação do JS não suporta essa operação => IEEE => gera imprecisão
+//console.log(10.toString()); // Conseguimos em cima do tipo number mas não do valor literal
+console.log((10.345).toFixed(2)); // entre parênteses o problema acima não existe
